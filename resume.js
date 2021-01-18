c0f95ee9957093a0a6197e336643f1086eb2353e
Vintour2020 <72978843+Vintour2020@users.noreply.github.com>
2021-01-19 01:09:27 +0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,56 @@
+function showIntroduction(){
+    let introduction=document.querySelector("#introduction");
+    introduction.style.display="block";
+}
+function closeIntroduction(){
+    let introduction=document.querySelector("#introduction");
+    introduction.style.display="none";
+}
+function showMotivation(){
+    let motivation=document.querySelector("#motivation");
+    motivation.style.display="block";
+}
+function closeMotivation(){
+    let motivation=document.querySelector("#motivation");
+    motivation.style.display="none";
+}
+function showEffort(){
+    let effort=document.querySelector("#effort");
+    effort.style.display="block";
+}
+function closeEffort(){
+    let effort=document.querySelector("#effort");
+    effort.style.display="none";
+}
+function showImagination(){
+    let imagination=document.querySelector("#imagination");
+    imagination.style.display="block";
+}
+function closeImagination(){
+    let imagination=document.querySelector("#imagination");
+    imagination.style.display="none";
+}
+function showArrangement(){
+    let arrangement=document.querySelector("#arrangement");
+    arrangement.style.display="block";
+}
+function closeArrangement(){
+    let arrangement=document.querySelector("#arrangement");
+    arrangement.style.display="none";
+}
+function showChallenge(){
+    let challenge=document.querySelector("#challenge");
+    challenge.style.display="block";
+}
+function closeChallenge(){
+    let challenge=document.querySelector("#challenge");
+    challenge.style.display="none";
+}
+function showWord(){
+    let word=document.querySelector("#word");
+    word.style.display="block";
+}
+function closeWord(){
+    let word=document.querySelector("#word");
+    word.style.display="none";
+}
